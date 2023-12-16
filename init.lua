d762a8dcc544ed606cecaaa95a717cc0fef3009d
tmrna <r.deans@outlook.com>
2023-12-15 22:10:10 -0500

    fix: typo in cmd GitBlameEnable

--- init.lua
+++ init.lua
@@ -533,7 +533,7 @@ cmp.setup {
 
 -- setting colorscheme
 vim.cmd.colorscheme 'gruvbox'
-vim.cmd 'GitBlameEnablet'
+vim.cmd 'GitBlameEnable'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
