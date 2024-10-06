8f1d2191bc8dc908fb3108a90cbd0d5094ff0a91
tmrna <tmrna_dev@outlook.com>
2024-10-06 11:19:40 -0400

    added keymaps for git stash

--- lua/plugins/git_blame.lua
+++ lua/plugins/git_blame.lua
@@ -6,5 +6,9 @@ return {
 	vim.api.nvim_set_keymap('n', '<leader>gof', ':GitBlameOpenFileURL<cr>',
 		{ desc = '[G]it blame [O]pen [F]ile URL' }),
 	vim.api.nvim_set_keymap('n', '<leader>gc', ':Git commit<CR>', {desc = '[G]it [C]ommit'}),
-	vim.api.nvim_set_keymap('n', '<leader>gaa', ':Git add *<CR>', {desc = '[G]it [A]dd [A]ll'})
+	vim.api.nvim_set_keymap('n', '<leader>gaa', ':Git add *<CR>', {desc = '[G]it [A]dd [A]ll'}),
+	vim.api.nvim_set_keymap('n', '<leader>grp', ':Git push<CR>', {desc = '[G]it [R]emote [P]ush'}),
+	vim.api.nvim_set_keymap('n', '<leader>gss', ':Git add *<CR>:Git stash<CR>', {desc = '[G]it [S]tash'}),
+	vim.api.nvim_set_keymap('n', '<leader>gsa', ':Git stash apply 0<CR>', {desc = '[G]it [S]tash [A]pply'}),
+	vim.api.nvim_set_keymap('n', '<leader>gsc', ':Git stash clear<CR>', {desc = '[G]it [S]tash [C]lear'})
 }
