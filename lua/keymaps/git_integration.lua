64206dc565ac20e4383dd847731908099b6f0f69
tmrna <tmrna_dev@outlook.com>
2024-10-08 20:49:51 -0400

    Added keymap for git mergetool

--- lua/keymaps/git_integration.lua
+++ lua/keymaps/git_integration.lua
@@ -4,5 +4,6 @@ return {
 	vim.api.nvim_set_keymap('n', '<leader>grp', ':Git push<CR>', {desc = '[G]it [R]emote [P]ush'}),
 	vim.api.nvim_set_keymap('n', '<leader>gss', ':Git add *<CR>:Git stash<CR>', {desc = '[G]it [S]tash'}),
 	vim.api.nvim_set_keymap('n', '<leader>gsa', ':Git stash apply 0<CR>', {desc = '[G]it [S]tash [A]pply'}),
-	vim.api.nvim_set_keymap('n', '<leader>gsc', ':Git stash clear<CR>', {desc = '[G]it [S]tash [C]lear'})
+	vim.api.nvim_set_keymap('n', '<leader>gsc', ':Git stash clear<CR>', {desc = '[G]it [S]tash [C]lear'}),
+	vim.api.nvim_set_keymap('n', '<leader>gmt', ':Git mergetool<CR>', {desc = '[G]it [M]ergetool'})
 }
