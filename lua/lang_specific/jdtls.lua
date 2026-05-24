88d9cb2bb21fb6dc5a96e4e12ba4b9de8b5591bd
tmrna <r.deans@outlook.com>
2026-05-24 14:28:38 -0400

    Removed the jdtls items and related items (files)

--- lua/lang_specific/jdtls.lua
+++
@@ -1,8 +0,0 @@
--- yoinked from https://github.com/bcampolo/nvim-starter-kit
-
--- Java LSP
-return {
-  -- https://github.com/mfussenegger/nvim-jdtls
-  'mfussenegger/nvim-jdtls',
-  ft = 'java', -- Enable only on .java file extensions
-}
